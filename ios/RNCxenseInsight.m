349575c6ef1c6af8d7df55c6c7a6777f2faa27b6
aliamin <aamin@gulfnews.com>
2020-07-16 14:47:11 +0400

    add methods

--- ios/RNCxenseInsight.m
+++ ios/RNCxenseInsight.m
@@ -1,13 +1,45 @@
+#import "RNCxenseInsight.h"
 #import "CxenseInsight.h"
+#import <CxenseInsightSDK/CxenseInsight.h>
 
 @implementation RNCxenseInsight
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE(RNCxenseInsight)
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(startTrackerWithName:(NSString *)name
+                  siteID:(nonnull NSString *)siteID
+                  dispatchMode:(CxenseInsightDispatchMode)dispatchMode
+                  networkRestriction:(CxenseInsightNetworkRestriction)networkRestriction
+                  consentOptions:(NSArray<NSString *> *)consentOptions
+                  automaticMetaDataTracking:(BOOL)automaticMetaDataTracking)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    CxenseInsightTracker *tracker = [CxenseInsight trackerWithName:name
+                                                            siteId:siteID];
+    [CxenseInsight setDispatchMode:dispatchMode];
+    [CxenseInsight setNetworkRestriction:networkRestriction];
+    [CxenseInsight setConsentOptions:consentOptions];
+    [CxenseInsight setAutoMetaInfTracking:automaticMetaDataTracking];
+}
+
+RCT_EXPORT_METHOD(trackPageViewEventWithURL:(NSString *)URL
+                  referringURL:(NSString *)referringURL
+                  siteID:(NSString *)siteID
+                  Name:(NSString *)name siteID:(NSString *)siteID)
+{
+    CxenseInsightEventBuilder *builder = [CxenseInsightEventBuilder pageViewEventWithURL:URL
+                                                                            referringURL:referringURL
+                                                                                  siteId:siteID];
+    [builder setCustomParameter:title forKey:@"title"];
+    NSDictionary *eventData = [builder build];
+    
+    [_tracker trackEvent:eventData name: completion:^(NSDictionary *event, NSError *error) {
+        if (error) {
+            NSLog(@"Error during event reporting: %@", error.description);
+        } else {
+            NSLog(@"Event with data = '%@' was reported", event);
+        }
+    }];
+    [CxenseInsight dispatch];
 }
 
 @end
